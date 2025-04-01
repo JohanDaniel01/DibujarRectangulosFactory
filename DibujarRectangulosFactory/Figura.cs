127b35a3f6995dec212f31e137f57f2271c50f01
YojanHernandez <johanx1000@gmail.com>
2025-04-01 17:47:08 -0500

    feat: add Figura class to map the global properties of the interface

---
+++ DibujarRectangulosFactory/Figura.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Drawing;
+
+namespace DibujarRectangulosFactory
+
+public abstract class Figura
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public Color Color { get; set; }
+
+    protected Figura(int x, int y, Color color)
+    {
+        X = x;
+        Y = y;
+        Color = color;
+    }
+
+    public abstract void Dibujar(Graphics g);
+}
