581cffeeec201ef06a9a576e36c7a25226ee18bc
YojanHernandez <johanx1000@gmail.com>
2025-04-01 17:50:24 -0500

    feat: add reactangulo class to draw the rectangle

--- DibujarRectangulosFactory/Figura.cs
+++ DibujarRectangulosFactory/Figura.cs
@@ -2,19 +2,20 @@
 using System.Drawing;
 
 namespace DibujarRectangulosFactory
-
-public abstract class Figura
 {
-    public int X { get; set; }
-    public int Y { get; set; }
-    public Color Color { get; set; }
-
-    protected Figura(int x, int y, Color color)
+    public abstract class Figura
     {
-        X = x;
-        Y = y;
-        Color = color;
-    }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Color Color { get; set; }
 
-    public abstract void Dibujar(Graphics g);
+        protected Figura(int x, int y, Color color)
+        {
+            X = x;
+            Y = y;
+            Color = color;
+        }
+
+        public abstract void Dibujar(Graphics g);
+    }
 }
---
+++ DibujarRectangulosFactory/Rectangulo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DibujarRectangulosFactory
+{
+    public class Rectangulo : Figura
+    {
+        private static int contador = 0; 
+        public static int Contador => contador;
+        public Rectangulo(int x, int y, Color color) : base(x, y, color)
+        {
+            contador++; // Incrementa el contador al crear una instancia
+        }
+
+        public override void Dibujar(Graphics g)
+        {
+            using (SolidBrush brush = new SolidBrush(Color))
+            {
+                g.FillRectangle(brush, X, Y, 100, 50); // Dibuja un rectángulo de tamaño fijo
+            }
+        }
+    }
+}
